3764b02f6c31b167942439b5b6a92e2bd4b99e19
NicholasKrouse <nicholasjameskrouse@gmail.com>
2024-10-14 15:41:10 -0400

    Adding browsing capability and context manager

---
+++ bookshelf/src/modules/ContextHandler.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { setContext } from 'svelte';
+    import Browse from "./browse.svelte";
+    import ExampleHome from './ExampleHome.svelte';
+    import GenreBrowse from './GenreBrowse.svelte';
+
+    export let books = [];
+
+    let activeModule = 'home';
+    let filteredGenre = 'All';
+    $: console.log(`activeModule changed to: ${activeModule}`);
+
+    function setActiveModule(module) {
+        activeModule = module;
+    }
+    function setFilteredGenre(genre) {
+        filteredGenre = genre;
+    }
+    setContext('setActiveModule', setActiveModule);
+    setContext('setFilteredGenre', setFilteredGenre);
+</script>
+
+<main>
+    {#if activeModule === 'home'}
+        <ExampleHome />
+    {/if}   
+    {#if activeModule === 'browse'}
+        <Browse books={books} />
+    {/if}
+    {#if activeModule === 'genre'}
+        <GenreBrowse genre={filteredGenre} books={books} />
+    {/if}   
+</main>
---
+++ bookshelf/src/modules/ExampleHome.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { getContext } from 'svelte';
+
+    const setActiveModule = getContext('setActiveModule');
+
+</script>
+
+<main>
+    <h1>Home</h1>
+    <button on:click={() => setActiveModule('browse')}>Browse</button>
+</main>
