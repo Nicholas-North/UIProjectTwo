98fe672d0c05436cf8aefeed923166e3147896ae
Nicholas Bryant <nbyellow14@yahoo.com>
2024-10-14 15:35:58 -0400

    Adding Search page

--- bookshelf/src/App.svelte
+++ bookshelf/src/App.svelte
@@ -1,10 +1,24 @@
 <main>
   <div class="row">
     <div class="BigCol">
-      <p>Picture goes here</p>
+      <img src="Shelf.png" alt="bookshelfs" width="500" height="500">
     </div>
     <div class="SmallCol">
       <p>UI goes here</p>
+      <input type="text" id="mySearch" placeholder="Search.." title="Type in a category">
+      <button id="SearchBtn">Submit</button>
+      <p id="tester"></p>
+      <ul id="myUL">
+        <li>Adele</li>
+        <li>Agnes</li>
+      
+        <li>Billy</li>
+        <li>Bob</li>
+      
+        <li>Calvin</li>
+        <li>Christina</li>
+        <li>Cindy</li>
+      </ul>
     </div>
   </div>
 </main>
